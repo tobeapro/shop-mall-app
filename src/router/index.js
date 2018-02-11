d313ca06afb1749cdded98cc882a94eb98bf9514
rk <471091127@qq.com>
2018-02-11 15:36:56 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '../components//views/home.vue'
 import Category from '../components/views/category.vue'
 import ShopCar from '../components/views/shopCar.vue'
+import User from '../components/views/user.vue'
 Vue.use(Router)
 export default new Router({
   routes:[
@@ -15,10 +16,16 @@ export default new Router({
       path:'/category',
       name:'category',
       component:Category
-    },{
+    },
+    {
       path:'/shopCar',
       name:'shopCar',
       component:ShopCar
+    },
+    {
+      path:'/user',
+      name:'user',
+      component:User
     }
   ]
 })
